8560629b5008c0fd0e22533d5d0a38e20a422761
Kevin Parenti <kevinparenti@hotmail.com>
2020-03-16 18:16:41 -0300

    x

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + ''));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/'));
+res.sendFile(path.join(__dirname+''));
 });
  
 // Start the app by listening on the default Heroku port
