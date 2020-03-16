8a503901e8be86beca06b16f5d9191eda0fcd818
Kevin Parenti <kevinparenti@hotmail.com>
2020-03-16 13:50:10 -0300

    make it better

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+const server = http.creteServer(app);
+
 app.set ('port' , process.env.PORT || 3000 );
 
 server.listen (app.get ('port') , () => {
-    console.log(  ` server on port { app.get('port')}`);
+    console.log(  `server on port ${ app.get('port')}`);
 
 });
